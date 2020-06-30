5dbab7c5949ff6120c7ef7833955292e1ff8e1a7
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-06-30 08:51:04 +0200

    no message

---
+++ src/Blauhaus.Errors/Handler/IErrorHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Blauhaus.Errors.Handler
+{
+    public interface IErrorHandler
+    {
+        Task HandleExceptionAsync(object sender, Exception exception);
+        Task HandleErrorAsync(string errorMessage);
+    }
+}
