c05312312c1ad4a86ef16c71d98b33a55315fc2f
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-07-29 14:06:50 +0200

    no message

---
+++ src/Blauhaus.Errors.Tests/Tests/ErrorTests/ExceptionToErrorTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Blauhaus.Errors.Extensions;
+using NUnit.Framework;
+
+namespace Blauhaus.Errors.Tests.Tests.ErrorTests
+{
+    public class ExceptionToErrorTests
+    {
+        [Test]
+        public void IF_Exception_Is_ErrorException_SHOULD_return_Error()
+        {
+            //Arrange
+            var errorException = new ErrorException(TestErrors.TestErrorTwo);
+
+            //Act
+            var result = errorException.ToError();
+
+            //Assert
+            Assert.That(result, Is.EqualTo(TestErrors.TestErrorTwo));
+        } 
+
+        [Test]
+        public void IF_Exception_Is_not_ErrorException_SHOULD_throw()
+        {
+            //Arrange
+            var ex = new Exception(TestErrors.TestErrorTwo.ToString());
+
+            //Act
+            Assert.Throws<ArgumentException>(() => ex.ToError());
+             
+        }
+    }
+}
