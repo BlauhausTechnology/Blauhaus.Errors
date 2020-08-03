5b8f6605b3ee82053eea37e4dad1725336cf0950
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-08-03 17:56:11 +0200

    no message

--- src/Blauhaus.Errors/ErrorException.cs
+++ src/Blauhaus.Errors/ErrorException.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Blauhaus.Errors.Extensions;
+using CSharpFunctionalExtensions;
 
 namespace Blauhaus.Errors
 {
@@ -9,9 +11,24 @@ namespace Blauhaus.Errors
     /// </summary>
     public class ErrorException : Exception
     {
-        public ErrorException(Error error, string message = "") : base(message)
+        public ErrorException(Error error, Exception innerException) : base(error.ToString(), innerException)
         {
             Error = error;
+        }
+
+        public ErrorException(Error error) : base(error.ToString())
+        {
+            Error = error;
+        }
+
+        public ErrorException(string serializedError) : base(serializedError)
+        {
+            Error = serializedError.ToError();
+        }
+        
+        public ErrorException(Result result) : base(result.Error)
+        {
+            Error = result.Error.ToError();
         }
 
         public Error Error { get; }
