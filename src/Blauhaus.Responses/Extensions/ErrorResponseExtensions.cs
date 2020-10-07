7b92c682a1ac67ed55e3dba17f68af00d25518cd
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-10-07 12:02:54 +0200

    no message

---
+++ src/Blauhaus.Responses/Extensions/ErrorResponseExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Blauhaus.Errors;
+using CSharpFunctionalExtensions;
+
+namespace Blauhaus.Responses.Extensions
+{
+    public static class ErrorResponseExtensions
+    {
+        public static Response ToResponse(this Error error)
+        {
+            return Response.Failure(error);
+        }
+
+        public static Response<T> ToResponse<T>(this Error error)
+        {
+            return Response.Failure<T>(error);
+        }
+    }
+}
---
+++ src/Blauhaus.Responses/Extensions/ResponseErrorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Blauhaus.Errors;
+using Blauhaus.Errors.Extensions;
+using CSharpFunctionalExtensions;
+
+namespace Blauhaus.Responses.Extensions
+{
+    public static class ResponseErrorExtensions
+    {
+        public static bool IsError(this Response result, Error error)
+        {
+            return result.IsFailure && result.Error.Equals(error);
+        }
+
+        public static bool IsError<T>(this Response<T> result, Error error)
+        {
+            return result.IsFailure && result.Error.Equals(error);
+        }
+    }
+}
