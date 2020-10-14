73bb010a40f78ce91c8b2eeb7b1517ced679fcbf
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-10-14 15:11:12 +0200

    no message

--- src/Blauhaus.Errors/Errors.cs
+++ src/Blauhaus.Errors/Errors.cs
@@ -3,5 +3,6 @@
     public static class Errors
     {
         public static Error Undefined = Error.Create("No definition exists for this error");
+        public static Error Unexpected(string errorMessage = "") => Error.Create("An unexpected error has occured" + errorMessage == "" ? "" : ": " + errorMessage);
     }
 }
