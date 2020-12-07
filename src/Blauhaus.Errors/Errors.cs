6bd69f4fa1fba7cba7cf7990bbfdc1c83ec29618
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-12-07 20:11:15 +0200

    no message

--- src/Blauhaus.Errors/Errors.cs
+++ src/Blauhaus.Errors/Errors.cs
@@ -1,9 +1,19 @@
-﻿namespace Blauhaus.Errors
+﻿using System;
+using System.Linq.Expressions;
+using Blauhaus.Common.Utils.Extensions;
+
+namespace Blauhaus.Errors
 {
     public static class Errors
     {
         public static Error Undefined = Error.Create("No definition exists for this error");
         public static Error Unexpected(string errorMessage = "") => Error.Create("An unexpected error has occured" + errorMessage == "" ? "" : ": " + errorMessage);
         public static Error Cancelled = Error.Create("The operation was cancelled");
+
+        public static Error RequiredValue() => Error.Create("A required parameter was not provided");
+        public static Error RequiredValue(string propertyName) => Error.Create($"A value is required for {propertyName}");
+        public static Error RequiredValue<T>() => Error.Create($"{typeof(T).Name} was missing a required value");
+        public static Error RequiredValue<T>(Expression<Func<T, object>> property) => Error.Create($"A value for the {property.ToPropertyName()} property on {typeof(T).Name} is required");
+
     }
 }
---
+++ src/Blauhaus.Errors/Extensions/ValidationExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Blauhaus.Errors.Extensions
+{
+    public static class ValidationExtensions
+    {
+        
+        public static string ThrowIfNullOrWhiteSpace(this string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ErrorException(Errors.RequiredValue());
+
+            return value;
+        }
+
+        public static string ThrowIfNullOrWhiteSpace<T>(this string value, Expression<Func<T, object>> property)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ErrorException(Errors.RequiredValue(property));
+
+            return value;
+        }
+
+        public static Guid ThrowIfEmpty(this Guid value)
+        {
+            if (value == Guid.Empty)
+                throw new ErrorException(Errors.RequiredValue());
+
+            return value;
+        }
+
+        public static Guid ThrowIfEmpty<T>(this Guid value, Expression<Func<T, object>> property)
+        {
+            if (value == Guid.Empty)
+                throw new ErrorException(Errors.RequiredValue<T>(property));
+
+            return value;
+        }
+    }
+}
